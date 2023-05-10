fb6a59ff30cfa9f59fd9af53104fbcc88c8950b7
nick-dolf <nickdolf@gmail.com>
2023-05-10 08:29:21 -0500

    added dotenv

--- treerat.js
+++ treerat.js
@@ -1,6 +1,14 @@
+require('dotenv').config()
 const PORT = 7533;
 const express = require("express");
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  console.log("Environment: Development");
+
+  // Make HTML easy to read in development
+  app.locals.pretty = true;
+}
 
 app.get("/", (req, res) => {
   res.send("TREE RAT");
