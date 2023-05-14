2b8f41bfffb9b799f0ced10ae8c8ed433ccc09d9
nick-dolf <nickdolf@gmail.com>
2023-05-14 09:00:05 -0500

    added live reload

--- treerat.js
+++ treerat.js
@@ -1,11 +1,24 @@
-require('dotenv').config()
+require("dotenv").config();
 const PORT = 7533;
 const express = require("express");
 const app = express();
 module.exports = app;
-const build = require("./admin/utils/build")
+const build = require("./admin/utils/build");
 
-build.setup("treerat.json")
+// Live Reload
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+build.setup("treerat.json");
 
 if (process.env.NODE_ENV === "development") {
   console.log("Environment: Development");
@@ -14,8 +27,8 @@ if (process.env.NODE_ENV === "development") {
   app.locals.pretty = true;
 }
 
-app.set("view engine", "ejs")
-app.set("views", ["admin/views", "views"])
+app.set("view engine", "ejs");
+app.set("views", ["admin/views", "views"]);
 
 app.get("/", (req, res) => {
   res.render("default");
