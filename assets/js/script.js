ad557607ad5570493a4978a7c2abb5d4eaf6027e
nick-dolf <nickdolf@gmail.com>
2023-08-29 20:23:36 -0500

    added header menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,51 @@
 /**
+ * Menus
+ */
+const header = document.querySelector(".site-header");
+
+// Toggle Mobile Dropdown Menu
+document.getElementById("menu-toggle").addEventListener("click", menuToggle);
+
+function menuToggle() {
+  header.classList.toggle("menu-clicked");
+  header.classList.remove("search-clicked");
+}
+
+// Toggle Sub Menu Dropdowns
+for (subMenuToggle of document.getElementsByClassName("sub-menu-toggle")) {
+  console.log(subMenuToggle)
+  subMenuToggle.addEventListener("click", toggleSubMenu);
+}
+
+function toggleSubMenu(event) {
+  console.log('sub')
+  event.currentTarget.parentNode.classList.toggle("sub-menu-clicked");
+}
+
+/**
  * Scroll Button function
  */
-let scrollButtons = document.getElementsByClassName('scroll')
-for (button of scrollButtons) {
-  button.addEventListener('click', scrollToNextSection)
+for (button of document.getElementsByClassName("scroll")) {
+  button.addEventListener("click", scrollToNextSection);
 }
 
 function scrollToNextSection(event) {
-  const index = event.currentTarget.dataset.scroll
-  const sections = document.getElementsByTagName('section')
-  sections[index].scrollIntoView({behavior: "smooth"})
+  const index = event.currentTarget.dataset.scroll;
+  const sections = document.getElementsByTagName("section");
+  sections[index].scrollIntoView({ behavior: "smooth" });
 }
 
-
-const header = document.querySelector(".site-header")
-
-function menuToggle() {
-  header.classList.toggle("menu-clicked")
-  header.classList.remove("search-clicked")
-}
-
-function subMenuToggle(event) {
-  event.currentTarget.parentNode.classList.toggle("sub-menu-clicked")
-}
-
+/**
+ * Search Toggles
+ */
 function searchToggle() {
-  header.classList.toggle("search-clicked")
-  header.classList.remove("menu-clicked")
+  header.classList.toggle("search-clicked");
+  header.classList.remove("menu-clicked");
 }
 
 const searchForm = document.querySelector(".form-search");
 const searchInput = document.querySelector(".search__input");
 function toggleSearchInput() {
-  searchForm.classList.toggle("active")
-  searchInput.focus()
+  searchForm.classList.toggle("active");
+  searchInput.focus();
 }
-
